fbd368dac6fdd0d98128aef306dc2143af947729
Noor Joomratty <noor@joomratty.com>
2023-09-07 15:44:14 +0200

    Create 16*16 Grid using JS to manipulate DOM

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const container = document.querySelector('#container');
+    
+    for (let i = 0; i < 16; i++) {
+        for (let j = 0; j < 16; j++) {
+            const grid = document.createElement('div');
+            grid.classList.add('grid');
+            container.appendChild(grid);
+        }
+    }
+});
+
